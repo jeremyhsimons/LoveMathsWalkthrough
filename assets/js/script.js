dc32e0bf37eebffb527a61b51054248bb2dab857
jeremyhsimons <jeremysimons15@gmail.com>
2023-01-12 11:31:38 +0000

    Add code for the multiply game type.

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,11 @@ function runGame(gameType) {
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-  } else {
+  } 
+  else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
+  }
+  else {
     alert(`Unkown game type: ${gameType}`);
     throw `Unkown game type: ${gameType}. Aborting!`
   }
@@ -73,6 +77,9 @@ function calculateCorrectAnswer() {
     if (operator === '+') {
         return [operand1 + operand2, 'addition'];
     }
+    else if (operator === "x") {
+        return [operand1 * operand2, 'multiply'];
+    }
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -101,15 +108,21 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '+';
+
 }
 
 function displaySubractQuestion() {
     
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x';
+
 }
