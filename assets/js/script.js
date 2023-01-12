69628bba916c18368bd40e46ea5ade0e54353d9d
jeremyhsimons <jeremysimons15@gmail.com>
2023-01-12 12:17:42 +0000

    add code to make division game funtional!

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,9 @@ function runGame(gameType) {
   else if (gameType === "subtract") {
     displaySubractQuestion(num1, num2);
   }
+  else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
+  }
   else {
     alert(`Unkown game type: ${gameType}`);
     throw `Unkown game type: ${gameType}. Aborting!`;
@@ -96,6 +99,9 @@ function calculateCorrectAnswer() {
     }
     else if (operator === "-") {
         return [operand1 - operand2, 'subtract'];
+    }
+    else if (operator === "/") {
+        return [operand1 / operand2, 'division'];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -146,4 +152,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = (operand1 *= operand2);
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById('operator').textContent = '/';
 }
